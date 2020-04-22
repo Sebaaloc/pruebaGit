54bdaf083cb4062f8636adac14b6ed791a93de7b
Sebaaloc <sebastianaloc@gmail.com>
2020-04-22 12:47:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 
-app.set('port', 4000)
+app.set('port', 5000)
 
 app.listen(app.get('port'), () => {
     console.log(`Eschuchando en puerto ${app.get('port')}`);
