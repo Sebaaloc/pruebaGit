0a5bc6601ab18f05912234a209ed69c473ae412f
Sebastian Alarcon <salarcon@sistecredito.com>
2020-04-22 13:11:36 -0500

    commit agregue consola

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.set('port', 3000)
 console.log('hola mundo')
 console.log('hello world')
 console.log('hello world')
+console.log('#########')
 
 app.listen(app.get('port'), () => {
     console.log(`Eschuchando en puerto ${app.get('port')}`);
