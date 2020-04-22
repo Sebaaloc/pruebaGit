c3fe0a3c4ecccd5592664f856a3a3fdee575dc82
Sebastian Alarcon <salarcon@sistecredito.com>
2020-04-22 13:15:45 -0500

    changing console.log

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ const express = require('express');
 const app = express()
 
 app.set('port', 3000)
-console.log('hola mundo')
-console.log('hello world')
-console.log('hello world')
-console.log('#########')
 
 app.listen(app.get('port'), () => {
     console.log(`Eschuchando en puerto ${app.get('port')}`);
